3ee1bb0d5b4fcdf03b08a2b7eb8b6713a97097ab
Cruzjonathan22 <cruzjonathan_bsit@plmun.edu.ph>
2025-02-10 15:32:37 +0800

    "Submitting Python Session 03 - Exercise 01"

--- problem_01.py
+++ problem_01.py
@@ -4,10 +4,8 @@
 # Simple Calculator Program
 
 def main():
-    
-    pass
+
     print("Simple Calculator Program")
-    
     num1 = int(input("Enter 1st number: "))
     num2 = int(input("Enter 2nd number: "))
     
